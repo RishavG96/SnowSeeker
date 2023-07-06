e339c6ab870bde43e8d640ce9b48efd41ffaaa3b
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-07-06 07:12:35 +0530

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Rishav Gupta on 06/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
